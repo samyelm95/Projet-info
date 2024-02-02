daee6befead75f4f47e39ffff9ec553f85831b5f
marzaguina <133380267+marzaguina@users.noreply.github.com>
2024-02-02 18:45:07 +0100

    Update prog_s.sh

--- Projet-info/Progc/prog_s.sh
+++ Projet-info/Progc/prog_s.sh
@@ -1,23 +1,16 @@
-#!/bin/bash
-# Création des fichiers nécessaires 
 fichier_resultats="Demo/prog_s.csv"
-
-
 # Utilisation de gnuplot
-gnuplot -persist <<EOF
-set terminal pngcairo enhanced font 'arial,10' size 1200, 600
-set output 'Image/histogramme_s.png'
-set title "Option -S : Distance = f(Route)"
-set ylabel 'DISTANCE (km)'
-set yrange [0:]
-set xtics rotate by 45 right #offset -3,-1.5
-set bmargin 3
-
-plot '$fichier_resultats' using 0:(\$2):(\$4) with filledcurve lc rgbcolor 'spring-green' title "Distance Max/Min (Km)",\
-    ''using 3:xtic(1) smooth mcspline lc rgbcolor 'dark-green' title "Distance average (Km)"
+gnuplot <<EOF
+    set terminal pngcairo enhanced font 'Times New Roman,12' size 800, 640
+    set datafile separator ";"
+    set xtic rotate by 45 font '0,8' offset -2,-2.5
+    set xlabel "Identifiants Trajets" font '0,12' offset 0,-2
+    set ylabel "Distance (km)" font '0,12'
+    set title 'Option -s'
+    set tics out nomirror
+    set output 'Image/histogramme_s.png'
+    plot '$fichier_resultats' using 0:(\$2):(\$4) with filledcurve lc rgbcolor 'pink' title "Distance Max/Min (Km)",\
+    '' using 3:xtic(1) smooth mcspline lc rgbcolor 'purple' title "Distance moyenne (Km)"
 EOF
-
 chmod 777 Image/histogramme_s.png
 xdg-open Image/histogramme_s.png
-
-
